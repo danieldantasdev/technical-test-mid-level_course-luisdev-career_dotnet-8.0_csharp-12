163f9a0fcfc038447f26efb9dc65c5d23b87ac10
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-14 19:55:09 -0300

    feat: implementação da extension de infra

---
+++ Jobs.CleanArchitecture/Jobs.CleanArchitecture.Infra/Extensions/InfrastrutureExtension.cs
@@ -0,0 +1,14 @@
+﻿using Jobs.CleanArchitecture.Core.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jobs.CleanArchitecture.Infra.Extensions
+{
+    public static class InfrastrutureExtension
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddScoped<ISqlConnectionFactoryService, ISqlConnectionFactoryService>();
+            return services;
+        }
+    }
+}
