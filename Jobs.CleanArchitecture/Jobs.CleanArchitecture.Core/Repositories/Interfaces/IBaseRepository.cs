3d8eeb11ac235238eba8f3ff90caf777116c981c
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-14 02:50:51 -0300

    feat: implementação das interfaces do repositório

---
+++ Jobs.CleanArchitecture/Jobs.CleanArchitecture.Core/Repositories/Interfaces/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobs.CleanArchitecture.Core.Repositories.Interfaces;
+
+internal interface IBaseRepository<T> where T : class
+{
+    Task<List<T>> GetAll();
+    Task<T> GetById(int id);
+    Task Add(T entity);
+    Task Update(T entity);
+    Task Delete(T entity);
+}
---
+++ Jobs.CleanArchitecture/Jobs.CleanArchitecture.Core/Repositories/Interfaces/IJobRepository.cs
@@ -0,0 +1,7 @@
+﻿using Jobs.CleanArchitecture.Core.Entities;
+
+namespace Jobs.CleanArchitecture.Core.Repositories.Interfaces;
+
+internal interface IJobRepository : IBaseRepository<Job>
+{
+}
