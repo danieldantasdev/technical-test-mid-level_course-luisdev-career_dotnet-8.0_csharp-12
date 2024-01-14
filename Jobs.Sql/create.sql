d31ada0658ef5d50a4643738bd6d6626fbd0d6e7
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-14 20:25:18 -0300

    feat: implementação comando de riação das tabelas

---
+++ Jobs.Sql/create.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[job] (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(50),
+    description VARCHAR(50),
+    location VARCHAR(50),
+    salary DECIMAL(18, 2),
+    id_status INT,
+    FOREIGN KEY (id_status) REFERENCES [dbo].[status](id)
+);
+
+GO;
+
+CREATE TABLE [dbo].[status] (
+    id INT NOT NULL PRIMARY KEY,
+    name INT,
+    description VARCHAR(50)
+);
