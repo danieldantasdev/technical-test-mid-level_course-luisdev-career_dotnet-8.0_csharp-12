a91c229bf8b5f423a1ba910c6f2f0b72e927e141
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-14 19:52:32 -0300

    feat: implementação da extension de infrastructure

--- Jobs.CleanArchitecture/Jobs.CleanArchitecture.Api/Program.cs
+++ Jobs.CleanArchitecture/Jobs.CleanArchitecture.Api/Program.cs
@@ -1,6 +1,9 @@
+using Jobs.CleanArchitecture.Infra.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
