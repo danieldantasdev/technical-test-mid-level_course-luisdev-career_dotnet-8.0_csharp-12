d9d73dfd6dfcacb1ebbd3fd6cfb4a55e9caf1d12
danieldantasdev <contatodanieldantasdev@gmail.com>
2024-01-14 19:54:50 -0300

    feat: implementação do service do sql factory

---
+++ Jobs.CleanArchitecture/Jobs.CleanArchitecture.Infra/Services/Implementations/SqlConnectionFactoryService.cs
@@ -0,0 +1,15 @@
+﻿using Jobs.CleanArchitecture.Core.Services.Interfaces;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace Jobs.CleanArchitecture.Infra.Services.Implementations;
+
+internal sealed class SqlConnectionFactoryService(IConfiguration configuration) : ISqlConnectionFactoryService
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public SqlConnection CreateConnection()
+    {
+        return new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
